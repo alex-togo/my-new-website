5590646d1d81a461ad08eaeb0a9d804c0d6457d5
Alex Togo <helloalextogo@gmail.com>
2020-12-10 16:07:19 -0500

    began title animation work

--- src/components/main-page/Intro.js
+++ src/components/main-page/Intro.js
@@ -3,63 +3,69 @@ import anime from "animejs/lib/anime.es.js";
 import Navbar from "./Navbar";
 
 const Intro = () => {
+  // set viewbox to bounding box to get rid of whitespace
+  // function setViewbox(svg) {
+  //   var bb = svg.getBBox();
+  //   svg.setAttribute(
+  //     "viewBox",
+  //     bb.x + "," + bb.y + "," + bb.width + "," + bb.height
+  //   );
+  // }
+
   // useEffect(() => {
-  // const dropColor = anime({
-  //   targets: ".circle",
-  //   translateY: 250,
-  //   duration: 2000,
-  //   complete: () => {
-  //     anime({
-  //       targets: ".intro",
-  //       backgroundColor: ["rgb(131, 131, 131)", "#000"],
-  //       duration: 4000,
-  //     });
-  //   },
-  // });
-  // const myName = anime({
-  //   targets: "#name-svg path",
-  //   loop: false,
-  //   strokeDashoffset: [anime.setDashoffset, 0],
-  //   easing: "linear",
-  //   direction: "alternate",
-  //   duration: 600,
-  //   delay: (el, i) => {
-  //     return i * 150;
-  //   },
-  //   complete: () => {
-  //     anime({
-  //       targets: "#name-svg path",
-  //       fill: "#ffffff",
-  //       duration: 4000,
-  //       complete: () => {
-  //         anime({
-  //           targets: "#heyIm",
-  //           display: "flex",
-  //         });
-  //       },
-  //     });
-  //   },
-  // });
-  // const dropColor = anime({
-  //   targets: ".circle",
-  //   translateY: 250,
-  //   duration: 500,
-  //   complete: () => {
-  //     anime({
-  //       targets: ".intro",
-  //       backgroundColor: ["rgb(131, 131, 131)", "#000"],
-  //       duration: 500,
-  //     });
-  //   },
-  // });
+  //   setViewbox(document.querySelector("svg"));
   // }, []);
+
+  // useEffect(() => {
+  //   anime({
+  //     targets: ".square",
+  //     width: "100%", // -> from '28px' to '100%',
+  //     easing: "easeInOutQuad",
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    var dr = anime
+      .timeline({
+        targets: ".darkRectangle",
+        autoplay: false,
+      })
+      .add({
+        duration: 400,
+        easing: "easeInOutExpo",
+        scaleX: [0.05, 0.05],
+        scaleY: [0, 1],
+      })
+      .add({
+        duration: 400,
+        easing: "easeInOutExpo",
+        scaleX: 1,
+      })
+      .add({
+        targets: ".title-text",
+        opacity: [0, 1],
+        delay: anime.stagger(400),
+      });
+
+    dr.play();
+  }, []);
 
   return (
     <section className="intro">
-      {/* <div className="circle"></div> */}
-      <div className="flex">
+      <div className="container flex">
         <Navbar />
       </div>
+      {/* <div className="square" style={{ position: "absolute" }}></div>
+      <p style={{ zIndex: "-1" }}>YO</p>
+      <div className="circle"></div> */}
+      <div className="container flex">
+        <div className="darkRectangle flex">
+          <p className="title-text">Hey,</p>
+          <p className="title-text">I'm</p>
+          <p className="title-text">Alex Togo</p>
+        </div>
+      </div>
+      {/* <div className="my-name">Alex Togo</div> */}
     </section>
   );
 };
