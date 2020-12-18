8433dc06fbae2dc3558f9ae4fe5a811ee0f98144
Alex Togo <helloalextogo@gmail.com>
2020-12-18 17:24:57 -0500

    got axios working with api and sendgrid

--- src/components/main-page/Contact.js
+++ src/components/main-page/Contact.js
@@ -1,104 +1,97 @@
 import React, { useState } from "react";
 import SectionTitle from "./SectionTitle";
-// import axios from "axios";
-// const nodemailer = require("nodemailer");
+import axios from "axios";
 
 const Contact = () => {
-  // const [person, setPerson] = useState({
-  //   name: "",
-  //   email: "",
-  //   message: "",
-  // });
+  const [person, setPerson] = useState({
+    name: "",
+    email: "",
+    message: "",
+    emailSent: null,
+  });
 
-  // const handleChange = (e) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   setPerson({ ...person, [name]: value });
-  // };
+  const errors = [];
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
 
-  //   // axios
-  //   //   .post("http://localhost:3030/api/email", person)
-  //   //   .then((res) => {
-  //   //     if (res.data.success) {
-  //   //       setPerson({ emailSent: true });
-  //   //     } else {
-  //   //       setPerson({ emailSent: false });
-  //   //     }
-  //   //   })
-  //   //   .catch((err) => {
-  //   //     setPerson({ emailSent: false });
-  //   //   });
-  //   let form = document.querySelector("#contact-form");
-  //   let successMsg = document.createTextNode("Your message was sent!");
-  //   form.appendChild(successMsg);
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  //   console.log(person);
-  // };
-
-  // const encode = (data) => {
-  //   return Object.keys(data)
-  //     .map(
-  //       (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-  //     )
-  //     .join("&");
-  // };
-
-  // const handleSubmit = (e) => {
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({ "form-name": "contact", ...person }),
-  //   })
-  //     .then(() => alert("Success!"))
-  //     .catch((error) => alert(error));
-
-  //   e.preventDefault();
-  // };
+    if (person.name && person.email && person.message) {
+      axios
+        .post("http://localhost:3030/api/email", person)
+        .then((res) => {
+          if (res.data.success) {
+            setPerson({ name: "", email: "", message: "", emailSent: true });
+          } else {
+            setPerson({ name: "", email: "", message: "", emailSent: false });
+          }
+        })
+        .catch((err) => {
+          setPerson({ emailSent: false });
+        });
+    } else {
+      errors.push("All fields must be filled in.");
+      setPerson({ ...person, emailSent: false });
+    }
+  };
 
   return (
     <section className="contact-me info-section" id="contact">
       <div className="container flex">
         <SectionTitle title={"Contact"} id="contact-title" />
         <form
-          netlify
-          name="contact"
           id="contact-form"
+          name="contact"
           className="flex wrap"
-          method="POST"
-          // onSubmit={handleSubmit}
+          method="post"
+          onSubmit={handleSubmit}
         >
-          <input type="hidden" name="form-name" value="contact" />
           {/* <label htmlFor="name">Name: </label> */}
           <input
             type="text"
             name="name"
             placeholder="Name"
-            // onChange={handleChange}
+            onChange={handleChange}
           ></input>
-
           {/* <label htmlFor="email">Email: </label> */}
           <input
             type="email"
             name="email"
             placeholder="Email"
-            // onChange={handleChange}
+            onChange={handleChange}
           ></input>
-
           {/* <label htmlFor="message">Message: </label> */}
           <textarea
+            type="text"
             name="message"
             placeholder="Type your message here."
             rows="5"
-            // onChange={handleChange}
+            onChange={handleChange}
           ></textarea>
-          {/* <div data-netlify-recaptcha="true"></div> */}
-
           <button type="submit" value="submit" className="submit-btn">
             Send
           </button>
+          {person.emailSent === true && (
+            <div style={{ color: "green" }}>Message sent!</div>
+          )}{" "}
+          {person.emailSent === false &&
+            errors.map((err, i) => {
+              return (
+                <div className="error" key={i}>
+                  {err}
+                </div>
+              );
+            })}
+          {/* {person.emailSent === true && (
+            <div style={{ margin: "10px 0", color: "green" }}>
+              Message sent!
+            </div>
+          )} */}
         </form>
       </div>
     </section>
